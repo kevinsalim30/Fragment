6120e1039aad54833bbe9734fcb615612c6275e7
kevinsalim30 <79781109+kevinsalim30@users.noreply.github.com>
2021-04-28 01:21:25 +0800

    first commit

---
+++ app/src/main/java/id/kevin/tabfragment4/Fragment1.java
@@ -0,0 +1,18 @@
+package id.kevin.tabfragment4;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1_layout,container,false);
+    }
+}
---
+++ app/src/main/java/id/kevin/tabfragment4/Fragment2.java
@@ -0,0 +1,17 @@
+package id.kevin.tabfragment4;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2_layout,container,false);
+    }
+}
